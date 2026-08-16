99c3d147b07817eac9c6ca1045c9d9aa1ee27761
thrive-spectrexq <frimpsbathrive@gmail.com>
2026-08-16 18:08:31 +0000

    feat(distribution): update Homebrew formula to v0.4.5 with multi-arch support & add sample Rhai modules

--- Formula/nxc-rs.rb
+++ Formula/nxc-rs.rb
@@ -1,21 +1,39 @@
 class NxcRs < Formula
   desc "NetExec-RS - High-performance offensive network orchestration framework"
   homepage "https://github.com/thrive-spectrexq/nxc-rs"
-  version "0.4.0"
+  version "0.4.5"
   
   if OS.mac?
-    url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.0/nxc-macos-amd64"
-    sha256 "REPLACE_WITH_SHA256"
+    if Hardware::CPU.arm?
+      url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.5/nxc-macos-arm64"
+      sha256 "REPLACE_WITH_SHA256"
+    else
+      url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.5/nxc-macos-amd64"
+      sha256 "REPLACE_WITH_SHA256"
+    end
   elsif OS.linux?
-    url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.0/nxc-linux-amd64"
-    sha256 "REPLACE_WITH_SHA256"
+    if Hardware::CPU.arm?
+      url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.5/nxc-linux-arm64"
+      sha256 "REPLACE_WITH_SHA256"
+    else
+      url "https://github.com/thrive-spectrexq/nxc-rs/releases/download/v0.4.5/nxc-linux-amd64"
+      sha256 "REPLACE_WITH_SHA256"
+    end
   end
 
   def install
     if OS.mac?
-      bin.install "nxc-macos-amd64" => "nxc"
+      if Hardware::CPU.arm?
+        bin.install "nxc-macos-arm64" => "nxc"
+      else
+        bin.install "nxc-macos-amd64" => "nxc"
+      end
     elsif OS.linux?
-      bin.install "nxc-linux-amd64" => "nxc"
+      if Hardware::CPU.arm?
+        bin.install "nxc-linux-arm64" => "nxc"
+      else
+        bin.install "nxc-linux-amd64" => "nxc"
+      end
     end
   end
 
